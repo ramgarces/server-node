cf7e21980b1d18650a279852125341f7555eb7ce
Ramiro Garces <ramgarces24@gmail.com>
2023-04-15 14:50:07 -0500

    Add departments endpoint

---
+++ src/routes/api/departments.js
@@ -0,0 +1,22 @@
+import {Router} from 'express'
+
+import { getDepartments, getDepartment } from '../../models/departments'
+import { getCompanies, getCompany, addCompany } from '../../models/companies'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+    const departments = await getDepartments()
+    res.send(departments)
+})
+
+router.get('/:id', async (req, res) => {
+    const department = await getDepartment(req.params.id)
+    if (department) {
+        res.send(department)
+    } else {
+        res.status(404).send({msg:'Company not found'})
+    }
+})
+
+export default router

d2237f679671da9a0971b5479ccf40b66e13428e
Ramiro Garces <ramgarces24@gmail.com>
2023-04-14 20:53:07 -0500

    Add companies

---
+++ src/models/companies.js
@@ -0,0 +1,7 @@
+import db from '../helpers/db'
+
+export const getCompanies = async () => db.company.findMany()
+
+export const getCompany = async (id) => db.company.findUnique({ where: { companyId: id } })
+
+export const addCompany = async (name) => db.company.create({data: {name}})

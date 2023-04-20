15efaec6e57b1be89cf6213b0c2947e7fc2d3f3a
Ramiro Garces <ramgarces24@gmail.com>
2023-04-20 17:15:06 -0500

    Add page and size

--- src/routes/api/employees.js
+++ src/routes/api/employees.js
@@ -10,7 +10,12 @@ import {getEmployee,
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const employees = await getEmployees()
+    // const {size, page} = req.query
+    const size = Number(req.query.size) || 10
+    const page = Number(req.query.page) || 1
+    const skip = size * (page - 1)
+    const take = size
+    const employees = await getEmployees(skip, take)
     res.send(employees)
 })
 
